1409a2bbee4cea6cc2885dddbaa7f8e8488211ef
Deepank <Sharmadeepank72@gmail.com>
2023-12-09 14:51:24 +0530

    This Is New Version

---
+++ app.js
@@ -0,0 +1,51 @@
+
+
+let inptEl = document.querySelector('input');
+let btnEl = document.querySelectorAll('button');
+
+for(let button of btnEl){
+    button.addEventListener('click', (e)=>{
+        let btnText = e.target.innerText;
+        if(btnText === 'AC'){
+            inptEl.value = '';
+        }
+        else if(btnText === '='){
+            try{
+                inptEl.value = eval(inptEl.value);
+            }
+            catch(err){
+                inptEl.value = 'Madarchod'
+            }
+        }
+        else{
+            inptEl.value = inptEl.value +
+            btnText;
+        }
+
+
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
